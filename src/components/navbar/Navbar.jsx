295565fd6633b988145f43cdb23a1e693eb66827
abar ahmed <abrarahmed2185@gmail.com>
2022-08-31 17:06:27 +0500

    Initial Commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT-3?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="navbar-links-container">
+          <Menu />
+        </div>
+      </div>
+      <div className="navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+
+      <div className="navbar-menu">
+        {toggle ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggle(false)}
+          />
+        ) : (
+          <RiMenu3Line color="#fff" size={27} onClick={() => setToggle(true)} />
+        )}
+        {toggle && (
+          <div className="navbar-menu-container scale-up-center">
+            <div className="navbar-menu-container-links">
+              <Menu />
+              <div className="navbar-menu-container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
+const Header = () => {
+  return (
+    <div className="header section__padding" id="home">
+      <div className="header-content" >
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="header-content-input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="header-content-people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="header-image">
+        <img src={ai} alt="robot" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
